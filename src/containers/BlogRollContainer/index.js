fbc1e00438bf0282570098f6ebbc37b457c0c8d0
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-17 19:37:26 -1000

    Printing all of the data out onto the blogroll, formatted with divs, added moment for the date

--- src/containers/BlogRollContainer/index.js
+++ src/containers/BlogRollContainer/index.js
@@ -1,37 +1,59 @@
 import React, { Component } from 'react';
 import $ from 'jquery';
+import moment from 'moment';
 
 class BlogRollContainer extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: []
+      posts: [],
+      users: []
     };
     this.getAllPosts = this.getAllPosts.bind(this);
+    this.getAllUsers = this.getAllUsers.bind(this);
   }
 
   getAllPosts() {
     $.get('http://localhost:8080/wp-json/wp/v2/posts').then(data => {
+      console.log(data, 'posts');
       this.setState({ posts: data });
+    });
+  }
+
+  getAllUsers() {
+    $.get('http://localhost:8080/wp-json/wp/v2/users').then(data => {
+      console.log(data, 'users');
+      this.setState({ users: data });
     });
   }
 
   componentDidMount() {
     this.getAllPosts();
+    this.getAllUsers();
   }
 
   render() {
-    console.log(this.state.posts, 'all posts data');
+    // console.log(this.state.posts, 'all posts data');
     return (
-      <div>
-        {this.state.posts.map(data => {
-          return (
-            <div>
-              {data.author}
-              {data.slug}
-            </div>
-          );
-        })}
+      <div className="blog-roll-container-outer">
+        <div className="blog-roll-container-inner">
+          {this.state.posts.map(data => {
+            return (
+              <div className="blog-item-container">
+                <h2 className="post-title">
+                  {data.title.rendered}
+                </h2>
+                <div
+                  className="post-excerpt"
+                  dangerouslySetInnerHTML={{ __html: data.excerpt.rendered }}
+                />
+                <div className="post-date">
+                  {moment(data.date).format('MMM DD, YYYY')}
+                </div>
+              </div>
+            );
+          })}
+        </div>
       </div>
     );
   }
