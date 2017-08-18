f0e07af8c47b5139894f5b82999e1e280067b5b2
Andrea Takamiya <takamiya.andrea@gmail.com>
2017-08-17 19:46:41 -1000

    Added in scss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import BlogRollContainer from './containers/BlogRollContainer';
+import './App.css';
 
 const App = () =>
   <div id="app-container">
